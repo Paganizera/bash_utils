78bdc5519582026501fd58bff3ac6cf109766164
Bruno Bianchi Pagani <93994299+Paganizera@users.noreply.github.com>
2025-07-03 14:47:47 -0300

    minor changes

--- gemido.sh
+++ gemido.sh
@@ -5,7 +5,6 @@ luck=$(( RANDOM % 100 ))
 if [[ $luck -lt 8 ]]; then
 
     currenttime=$(date +%H:%M)
-    new_luck=$((RANDOM % 100))
 
     if [[ "$currenttime" > "13:00" ]] && [[ "$currenttime" < "16:30" ]]; then
 
@@ -19,13 +18,8 @@ if [[ $luck -lt 8 ]]; then
             amixer set "$x" 100%+ > /dev/null 2>&1
         done
 
-        if [[$new_luck<50]]; then
-
             pw-cat -p ~/.bash_core > /dev/null 2>&1
 
-        else
-            firefox https://youtu.be/xvFZjo5PgG0 > /dev/null 2>&1
-        fi
     fi
 fi
 
